92b64fd6e9f0297deda14db12f44129a949a33b2
Alessa <37335212+alessa0008@users.noreply.github.com>
2018-04-20 02:58:28 +0700

    Create main.cpp

---
+++ source/main.cpp
@@ -0,0 +1,368 @@
+#include "main.h" 
+
+typedef HRESULT(APIENTRY *SetStreamSource_t)(IDirect3DDevice9*, UINT, IDirect3DVertexBuffer9*, UINT, UINT);
+HRESULT APIENTRY SetStreamSource_hook(IDirect3DDevice9*, UINT, IDirect3DVertexBuffer9*, UINT, UINT);
+SetStreamSource_t SetStreamSource_orig = 0;
+
+typedef HRESULT(APIENTRY *SetTexture_t)(IDirect3DDevice9*, DWORD, IDirect3DBaseTexture9 *);
+HRESULT APIENTRY SetTexture_hook(IDirect3DDevice9*, DWORD, IDirect3DBaseTexture9 *);
+SetTexture_t SetTexture_orig = 0;
+
+typedef HRESULT(APIENTRY* Present_t) (IDirect3DDevice9*, const RECT *, const RECT *, HWND, const RGNDATA *);
+HRESULT APIENTRY Present_hook(IDirect3DDevice9*, const RECT *, const RECT *, HWND, const RGNDATA *);
+Present_t Present_orig = 0;
+
+typedef HRESULT(APIENTRY *Reset_t)(IDirect3DDevice9*, D3DPRESENT_PARAMETERS*);
+HRESULT APIENTRY Reset_hook(IDirect3DDevice9*, D3DPRESENT_PARAMETERS*);
+Reset_t Reset_orig = 0;
+
+typedef HRESULT(APIENTRY *GetRenderTargetData_t)(IDirect3DDevice9*, IDirect3DSurface9 *pRenderTarget, IDirect3DSurface9 *pDestSurface);
+GetRenderTargetData_t oGetRenderTargetData = 0;
+
+typedef HRESULT(APIENTRY *CreateOffscreenPlainSurface_t)(IDirect3DDevice9*, UINT Width, UINT Height, D3DFORMAT Format, D3DPOOL Pool, IDirect3DSurface9 **ppSurface, HANDLE *pSharedHandle);
+CreateOffscreenPlainSurface_t oCreateOffscreenPlainSurface = 0;
+
+//==========================================================================================================================
+
+HRESULT APIENTRY hkGetRenderTargetData(LPDIRECT3DDEVICE9 pDevice, IDirect3DSurface9 *pRenderTarget, IDirect3DSurface9 *pDestSurface)
+{
+	screenshot_taken = true;
+	return oGetRenderTargetData(pDevice, pRenderTarget, pDestSurface);
+}
+
+//==========================================================================================================================
+
+HRESULT APIENTRY hkCreateOffscreenPlainSurface(LPDIRECT3DDEVICE9 pDevice, UINT Width, UINT Height, D3DFORMAT Format, D3DPOOL Pool, IDirect3DSurface9 **ppSurface, HANDLE *pSharedHandle)
+{
+	screenshot_taken = true;
+	Width = 1;
+	Height = 1;
+	Log("Screenshot blocked.");
+
+	return oCreateOffscreenPlainSurface(pDevice, Width, Height, Format, Pool, ppSurface, pSharedHandle);
+}
+
+//==========================================================================================================================
+
+HRESULT APIENTRY SetStreamSource_hook(LPDIRECT3DDEVICE9 pDevice, UINT StreamNumber, IDirect3DVertexBuffer9* pStreamData, UINT OffsetInBytes, UINT sStride)
+{
+	if (StreamNumber == 0)
+		Stride = sStride;
+
+	return SetStreamSource_orig(pDevice, StreamNumber, pStreamData, OffsetInBytes, sStride);
+}
+
+//==========================================================================================================================
+
+HRESULT APIENTRY SetTexture_hook(LPDIRECT3DDEVICE9 pDevice, DWORD Sampler, IDirect3DBaseTexture9 *pTexture)
+{
+	if (InitOnce)
+	{
+		InitOnce = false;
+				
+		pDevice->GetViewport(&Viewport);
+		ScreenCX = (float)Viewport.Width / 2.0f;
+		ScreenCY = (float)Viewport.Height / 2.0f;
+
+		LoadCfg();
+	}
+
+	
+	if (SUCCEEDED(pDevice->GetVertexShader(&vShader)))
+		if (vShader != NULL)
+			if (SUCCEEDED(vShader->GetFunction(NULL, &vSize)))
+				if (vShader != NULL) { vShader->Release(); vShader = NULL; }
+
+	if (wallhack>0)
+	{
+		pDevice->SetRenderState(D3DRS_DEPTHBIAS, 0);
+		if ((vSize == 2356 || vSize == 900 ||
+			vSize == 2008 || vSize == 640) || (Stride == 36 && vSize == 1436) || (Stride == 48 && vSize == 1436))
+		{
+			if (wallhack == 2 && vSize != 1436)
+			{
+				float sColor[4] = { 0.0f, 1.0f, 0.0f, 1.0f };
+				pDevice->SetPixelShaderConstantF(0, sColor, 1);
+				
+			}
+
+			float bias = 1000.0f;
+			float bias_float = static_cast<float>(-bias);
+			bias_float /= 10000.0f;
+			pDevice->SetRenderState(D3DRS_DEPTHBIAS, *(DWORD*)&bias_float);
+		}
+	}
+
+	if (aimbot == 1 || esp > 0)
+	{
+		if ((Stride == 48 && vSize > 1328) || (vSize == 2356 || vSize == 2008 || vSize == 1552))
+			AddWeapons(pDevice);
+	}
+		
+	return SetTexture_orig(pDevice, Sampler, pTexture);
+}
+
+//==========================================================================================================================
+
+HRESULT APIENTRY Present_hook(IDirect3DDevice9* pDevice, const RECT *pSourceRect, const RECT *pDestRect, HWND hDestWindowOverride, const RGNDATA *pDirtyRegion)
+{
+	if (GetAsyncKeyState(VK_ESCAPE) & 1 || GetAsyncKeyState(VK_INSERT))
+	{
+		
+		pDevice->GetViewport(&Viewport);
+		ScreenCX = (float)Viewport.Width / 2.0f;
+		ScreenCY = (float)Viewport.Height / 2.0f;
+	}
+
+	
+	if (Font == NULL)
+		D3DXCreateFont(pDevice, 14, 0, FW_BOLD, 0, FALSE, DEFAULT_CHARSET, OUT_DEFAULT_PRECIS, DEFAULT_QUALITY, DEFAULT_PITCH | FF_DONTCARE, TEXT("Italic"), &Font);
+
+	//if (ShowMenu)
+		//DrawBox(pDevice, 71.0f, 86.0f, 200.0f, 400.0f, D3DCOLOR_ARGB(200, 128, 128, 128));//black
+
+	if (Font)
+		DrawMenu(pDevice);
+
+	if (screenshot_taken && Font)
+	{
+		DrawCenteredString(Font, (int)Viewport.Width / 2, (int)Viewport.Height / 2, D3DCOLOR_ARGB(255, 255, 255, 255), "Someone reported you. Screenshot blocked. (gmcomplaint.jpg)");
+		static DWORD lastTime = timeGetTime();
+		DWORD timePassed = timeGetTime() - lastTime;
+		if (timePassed>2000)
+		{
+			screenshot_taken = false;
+			LoadCfg();
+			lastTime = timeGetTime();
+		}
+
+	}
+	
+	if (aimkey == 0) Daimkey = 0;
+	if (aimkey == 1) Daimkey = VK_SHIFT;
+
+									 
+	if (esp > 0 && WeaponEspInfo.size() != NULL)
+	{
+		for (unsigned int i = 0; i < WeaponEspInfo.size(); i++)
+		{
+			
+			if (WeaponEspInfo[i].pOutX > 1.0f && WeaponEspInfo[i].pOutY > 1.0f && (float)WeaponEspInfo[i].RealDistance > 4.0f)
+				DrawCornerBox(pDevice, (int)WeaponEspInfo[i].pOutX, (int)WeaponEspInfo[i].pOutY + 20, 20, 30, 1, D3DCOLOR_ARGB(255, 255, 255, 255));
+			if (WeaponEspInfo[i].pOutX > 1.0f && WeaponEspInfo[i].pOutY > 1.0f && (float)WeaponEspInfo[i].RealDistance > 200.0f)
+				DrawCenteredString(Font, (int)WeaponEspInfo[i].pOutX, (int)WeaponEspInfo[i].pOutY - 20.0f, D3DCOLOR_ARGB(255, 255, 255, 255), "%.f", (float)WeaponEspInfo[i].RealDistance);
+			else if (WeaponEspInfo[i].pOutX > 1.0f && WeaponEspInfo[i].pOutY > 1.0f && (float)WeaponEspInfo[i].RealDistance > 4.0f && (float)WeaponEspInfo[i].RealDistance <= 200.0f)
+				DrawCenteredString(Font, (int)WeaponEspInfo[i].pOutX, (int)WeaponEspInfo[i].pOutY - 20.0f, D3DCOLOR_ARGB(255, 255, 255, 0), "%.f", (float)WeaponEspInfo[i].RealDistance);
+
+		}
+	}
+
+	if (line > 0 && WeaponEspInfo.size() != NULL)
+	{
+		for (unsigned int i = 0; i < WeaponEspInfo.size(); i++)
+		{
+			if (WeaponEspInfo[i].pOutX > 1.0f && WeaponEspInfo[i].pOutY > 1.0f && (float)WeaponEspInfo[i].RealDistance > 4.0f)
+				DrawLine(pDevice, (int)WeaponEspInfo[i].pOutX, (int)WeaponEspInfo[i].pOutY, ScreenCX, ScreenCY * ((float)esp * 0.2f), 20, D3DCOLOR_ARGB(255, 255, 255, 255), 1);
+			else
+				if (WeaponEspInfo[i].pOutX > 1.0f && WeaponEspInfo[i].pOutY > 1.0f && (float)WeaponEspInfo[i].RealDistance > 4.0f && (float)WeaponEspInfo[i].vSizeod != 2008)
+					DrawLine(pDevice, (int)WeaponEspInfo[i].pOutX, (int)WeaponEspInfo[i].pOutY, ScreenCX, ScreenCY * ((float)esp * 0.2f), 20, D3DCOLOR_ARGB(255, 0, 255, 0), 1);
+		}
+	}
+
+	
+	if (aimbot == 1 && WeaponEspInfo.size() != NULL)
+	{
+		UINT BestTarget = -1;
+		DOUBLE fClosestPos = 99999;
+
+		for (unsigned int i = 0; i < WeaponEspInfo.size(); i++)
+		{
+			
+			float radiusx = (aimfov*5.0f) * (ScreenCX / 100.0f);
+			float radiusy = (aimfov*5.0f) * (ScreenCY / 100.0f);
+
+			if (aimfov == 0)
+			{
+				radiusx = 5.0f * (ScreenCX / 100.0f);
+				radiusy = 5.0f * (ScreenCY / 100.0f);
+			}
+
+			
+			WeaponEspInfo[i].CrosshairDistance = GetDistance(WeaponEspInfo[i].pOutX, WeaponEspInfo[i].pOutY, ScreenCX, ScreenCY);
+
+			
+			if (WeaponEspInfo[i].pOutX >= ScreenCX - radiusx && WeaponEspInfo[i].pOutX <= ScreenCX + radiusx && WeaponEspInfo[i].pOutY >= ScreenCY - radiusy && WeaponEspInfo[i].pOutY <= ScreenCY + radiusy)
+
+				
+				if (WeaponEspInfo[i].CrosshairDistance < fClosestPos)
+				{
+					fClosestPos = WeaponEspInfo[i].CrosshairDistance;
+					BestTarget = i;
+				}
+		}
+
+
+		
+		if (BestTarget != -1 && WeaponEspInfo[BestTarget].RealDistance > 4.0f)
+		{
+			double DistX = WeaponEspInfo[BestTarget].pOutX - ScreenCX;
+			double DistY = WeaponEspInfo[BestTarget].pOutY - ScreenCY;
+
+			DistX /= (0.5f + (float)aimsens*0.5f);
+			DistY /= (0.5f + (float)aimsens*0.5f);
+
+			
+			if (GetAsyncKeyState(Daimkey) & 0x8000)
+				mouse_event(MOUSEEVENTF_MOVE, (float)DistX, (float)DistY, 0, NULL);
+
+						
+		}
+	}
+	WeaponEspInfo.clear();
+		
+	return Present_orig(pDevice, pSourceRect, pDestRect, hDestWindowOverride, pDirtyRegion);
+}
+
+//==========================================================================================================================
+
+HRESULT APIENTRY Reset_hook(IDirect3DDevice9* pDevice, D3DPRESENT_PARAMETERS *pPresentationParameters)
+{
+	if (Font)
+		Font->OnLostDevice();
+
+	HRESULT ResetReturn = Reset_orig(pDevice, pPresentationParameters);
+
+	if (SUCCEEDED(ResetReturn))
+	{
+		if (Font)
+			Font->OnResetDevice();
+
+		InitOnce = true;
+	}
+
+	return ResetReturn;
+}
+
+//==========================================================================================================================
+
+HANDLE(WINAPI *Real_CreateFile)(LPCWSTR lpFileName, DWORD dwDesiredAccess, DWORD dwShareMode, LPSECURITY_ATTRIBUTES lpSecurityAttributes, DWORD dwCreationDisposition, DWORD dwFlagsAndAttributes, HANDLE hTemplateFile) = CreateFileW;
+HANDLE WINAPI Routed_CreateFile(LPCWSTR lpFileName, DWORD dwDesiredAccess, DWORD dwShareMode, LPSECURITY_ATTRIBUTES lpSecurityAttributes, DWORD dwCreationDisposition, DWORD dwFlagsAndAttributes, HANDLE hTemplateFile)
+{
+	char buffer[500];
+	wcstombs(buffer, lpFileName, 500);
+	if (strcmp(buffer + strlen(buffer) - 4, ".jpg") == 0)
+	{
+		Log("buffer == %s", buffer);
+	}
+	return Real_CreateFile(lpFileName, dwDesiredAccess, dwShareMode, lpSecurityAttributes, dwCreationDisposition, dwFlagsAndAttributes, hTemplateFile);
+}
+
+//==========================================================================================================================
+
+DWORD WINAPI alessapublic(__in LPVOID lpParameter)
+{
+	HMODULE dDll = NULL;
+	while (!dDll)
+	{
+		dDll = GetModuleHandleA("d3d9.dll");
+		Sleep(100);
+	}
+	CloseHandle(dDll);
+
+	IDirect3D9* d3d = NULL;
+	IDirect3DDevice9* d3ddev = NULL;
+
+	HWND tmpWnd = CreateWindowA("BUTTON", "alessapublic", WS_SYSMENU | WS_MINIMIZEBOX, CW_USEDEFAULT, CW_USEDEFAULT, 300, 300, NULL, NULL, Hand, NULL);
+	if (tmpWnd == NULL)
+	{
+		return 0;
+	}
+
+	d3d = Direct3DCreate9(D3D_SDK_VERSION);
+	if (d3d == NULL)
+	{
+		DestroyWindow(tmpWnd);
+		
+		return 0;
+	}
+
+	D3DPRESENT_PARAMETERS d3dpp;
+	ZeroMemory(&d3dpp, sizeof(d3dpp));
+	d3dpp.Windowed = TRUE;
+	d3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD;
+	d3dpp.hDeviceWindow = tmpWnd;
+	d3dpp.BackBufferFormat = D3DFMT_UNKNOWN;
+
+	HRESULT result = d3d->CreateDevice(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, tmpWnd, D3DCREATE_SOFTWARE_VERTEXPROCESSING, &d3dpp, &d3ddev);
+	if (result != D3D_OK)
+	{
+		d3d->Release();
+		DestroyWindow(tmpWnd);
+		
+		return 0;
+	}
+
+	
+#if defined _M_X64
+	DWORD64* dVtable = (DWORD64*)d3ddev;
+	dVtable = (DWORD64*)dVtable[0];
+#elif defined _M_IX86
+	DWORD* dVtable = (DWORD*)d3ddev;
+	dVtable = (DWORD*)dVtable[0]; 
+#endif
+				  							  
+	if (MH_Initialize() != MH_OK) { return 1; }
+	if (MH_CreateHook((DWORD_PTR*)dVtable[17], &Present_hook, reinterpret_cast<void**>(&Present_orig)) != MH_OK) { return 1; }
+	if (MH_EnableHook((DWORD_PTR*)dVtable[17]) != MH_OK) { return 1; }
+	if (MH_CreateHook((DWORD_PTR*)dVtable[100], &SetStreamSource_hook, reinterpret_cast<void**>(&SetStreamSource_orig)) != MH_OK) { return 1; }
+	if (MH_EnableHook((DWORD_PTR*)dVtable[100]) != MH_OK) { return 1; }
+	if (MH_CreateHook((DWORD_PTR*)dVtable[65], &SetTexture_hook, reinterpret_cast<void**>(&SetTexture_orig)) != MH_OK) { return 1; }
+	if (MH_EnableHook((DWORD_PTR*)dVtable[65]) != MH_OK) { return 1; }
+	if (MH_CreateHook((DWORD_PTR*)dVtable[16], &Reset_hook, reinterpret_cast<void**>(&Reset_orig)) != MH_OK) { return 1; }
+	if (MH_EnableHook((DWORD_PTR*)dVtable[16]) != MH_OK) { return 1; }
+
+	if (MH_CreateHook((DWORD_PTR*)dVtable[32], &hkGetRenderTargetData, reinterpret_cast<void**>(&oGetRenderTargetData)) != MH_OK) { return 1; }
+	if (MH_EnableHook((DWORD_PTR*)dVtable[32]) != MH_OK) { return 1; }
+	if (MH_CreateHook((DWORD_PTR*)dVtable[36], &hkCreateOffscreenPlainSurface, reinterpret_cast<void**>(&oCreateOffscreenPlainSurface)) != MH_OK) { return 1; }
+	if (MH_EnableHook((DWORD_PTR*)dVtable[36]) != MH_OK) { return 1; }
+
+	HMODULE modd = LoadLibrary(TEXT("Kernel32.dll"));
+	void* ptrr = GetProcAddress(modd, "CreateFileW");
+	MH_CreateHook(ptrr, Routed_CreateFile, reinterpret_cast<void**>(&Real_CreateFile));
+	MH_EnableHook(ptrr);
+		
+	d3ddev->Release();
+	d3d->Release();
+	DestroyWindow(tmpWnd);
+
+	return 1;
+}
+
+//==========================================================================================================================
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
+{
+	switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH:
+		Hand = hModule;
+		DisableThreadLibraryCalls(hModule); 
+		GetModuleFileNameA(hModule, dlldir, 512);
+		for (int i = (int)strlen(dlldir); i > 0; i--)
+		{
+			if (dlldir[i] == '\\')
+			{
+				dlldir[i + 1] = 0;
+				break;
+			}
+		}
+		CreateThread(0, 0, alessapublic, 0, 0, 0);
+
+		break;
+	case DLL_PROCESS_DETACH:
+		break;
+	case DLL_THREAD_ATTACH:
+	case DLL_THREAD_DETACH:
+		break;
+	}
+	return TRUE;
+}
